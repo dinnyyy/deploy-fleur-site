2f2ca7c51a14e91fdea98ecaf0e9255ddf088419
dinnyyy <joshdinn01@gmail.com>
2024-08-30 00:40:58 +1000

    datechange#2

--- src/pages/countdownPage.jsx
+++ src/pages/countdownPage.jsx
@@ -2,7 +2,7 @@ import Countdown from "../components/countdown";
 /*import { Link } from "react-router-dom";
  */
 export function CountdownPage() {
-  const targetDate = "2024-08-20T00:00:00";
+  const targetDate = "2024-09-20T00:00:00";
   return (
     <div className="timer">
       <div className="wrapper">
